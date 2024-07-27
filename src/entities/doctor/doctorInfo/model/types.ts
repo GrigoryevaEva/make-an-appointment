7da58175a2bb800a0b872cc9dd17ce7ac5f8949b
Eva <grigoryeva.eva.2704@mail.ru>
2024-07-28 02:35:57 +0300

    add fetchDoctorInfo

--- src/entities/doctor/doctorInfo/model/types.ts
+++ src/entities/doctor/doctorInfo/model/types.ts
@@ -1,11 +1,14 @@
 import { IDoctorInfo } from "@/shared/api/doctor/doctorInfo/types"
+import { RejectedDataType } from "@/shared/types/error"
 
 export type State = {
   readonly doctorInfo: IDoctorInfo
   readonly loading: boolean
-  readonly error: null
+  readonly error: null | RejectedDataType
 }
 
 export type Actions = {}
 
-export type Thunks = {}
+export type Thunks = {
+  readonly fetchDoctorInfo: () => void
+}
