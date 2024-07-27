cb8b992b8b17f6c55485321dfa6fd5e5226ff7a4
Eva <grigoryeva.eva.2704@mail.ru>
2024-07-28 01:47:56 +0300

    add widget freeSessionsList

---
+++ src/widgets/freeSessionsList/index.ts
@@ -0,0 +1,1 @@
+export { FreeSessionsList } from "./ui/freeSessionsList";
---
+++ src/widgets/freeSessionsList/ui/freeSessionsList.tsx
@@ -0,0 +1,7 @@
+import style from './style.module.scss'
+
+export const FreeSessionsList = () => {
+  return (
+    <div>freeSessionsList</div>
+  )
+}
