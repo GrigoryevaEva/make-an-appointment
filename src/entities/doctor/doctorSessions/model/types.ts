0e9c24782a0f84c41dde9718547cceb952ea72c0
Eva <grigoryeva.eva.2704@mail.ru>
2024-07-28 19:27:37 +0300

    create fetchDoctorSessions

--- src/entities/doctor/doctorSessions/model/types.ts
+++ src/entities/doctor/doctorSessions/model/types.ts
@@ -1,11 +1,14 @@
 import { IDoctorSessions } from "@/shared/api/doctor/doctorSessions/types"
+import { RejectedDataType } from "@/shared/types/error"
 
 export type State = {
   readonly doctorSessions: IDoctorSessions
   readonly loading: boolean
-  readonly error: null
+  readonly error: null | RejectedDataType
 }
 
 export type Actions = {}
 
-export type Thunks = {}
+export type Thunks = {
+  readonly fetchDoctorSessions: () => void
+}
--- src/entities/doctor/doctorSessions/model/useDoctorSessions.ts
+++ src/entities/doctor/doctorSessions/model/useDoctorSessions.ts
@@ -1,5 +1,9 @@
 import { create } from 'zustand'
 import { State, Actions, Thunks } from './types'
+import { ErrorType } from '@/shared/types/error'
+import { getDoctorSessions } from '@/shared/api/doctor/doctorSessions/doctorSessions'
+import { dateToUnix } from '@/shared/function'
+import { sessionUnixSize } from '@/shared/constants'
 
 export const useDoctorSessions = create<State & Actions & Thunks>(set => ({
   doctorSessions: {
@@ -10,6 +14,57 @@ export const useDoctorSessions = create<State & Actions & Thunks>(set => ({
     busyTime: [],
     freeSessions: [],
   },
-  loading: false,
+  loading: true,
   error: null,
+  fetchDoctorSessions: async () => {
+    try {
+      const resp = await getDoctorSessions()
+      set({loading: false})
+
+      const startWorking = dateToUnix(`${resp.workingHours.start}`)
+      const stopWorking = dateToUnix(`${resp.workingHours.stop}`)
+      const lastSession = stopWorking - sessionUnixSize
+
+      const busyTime = resp.busyTime.map((i) => (
+        {
+          start: dateToUnix(`${i.start}`),
+          stop: dateToUnix(`${i.stop}`),
+        }
+      )).sort((a, b) => (a.start - b.start))
+
+      const freeSessions = []
+
+      label: for (let startSession = startWorking; startSession <= lastSession;) {
+        let stopSession = startSession + sessionUnixSize
+    
+        for (let i of busyTime) {
+          if (i.start >= startSession && i.start < stopSession ) {
+            startSession = i.stop
+            continue label
+          }
+        }
+    
+        freeSessions.push(startSession)
+        startSession = stopSession
+      }
+
+      set({
+        doctorSessions: {
+          workingHours: {
+            start: startWorking,
+            stop: stopWorking,
+          },
+          busyTime: busyTime,
+          freeSessions: freeSessions,
+        }
+      })
+    } catch (err: unknown) {
+      set({loading: false})
+      const error = err as ErrorType
+      set({ error: {
+        messageError: error.message,
+        status: error.response?.status
+      } })
+    }
+  }
 }))
